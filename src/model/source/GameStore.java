72213cd9099dd5100e0c14666825a4cec6b5694b
CamiloCJ09 <camilocampazj@gmail.com>
2021-09-22 08:59:06 -0500

    Merge

--- src/model/source/GameStore.java
+++ src/model/source/GameStore.java
@@ -1,5 +1,7 @@
 package model.source;
 
+import model.ownImplementation.classes.*;
+
 import java.util.ArrayList;
 
 public class GameStore{
@@ -7,4 +9,11 @@ public class GameStore{
     public ArrayList<Game> storeGames;
     public ArrayList<Publication> review;
     public ArrayList<Publication> critics;
+
+    public StackList<Game> userGames;
+
+    public GameStore(){
+
+    }
+    public
 }
