822e0af319a101faf866dcb7d1e188c71465f2c2
Johan794 <johan.ricardo1104@gmail.com>
2021-09-24 07:12:38 -0500

    Hash table implemented

--- src/model/ownImplementation/classes/HashTable.java
+++ src/model/ownImplementation/classes/HashTable.java
@@ -5,32 +5,41 @@ import model.ownImplementation.interfaces.IHashTable;
 public class HashTable<K,V>  implements IHashTable<K,V> {
 
     private HashNode<K, V> node;
+    private int size;
 
     public HashTable(int size) {
+        this.size = size;
         node = new HashNode<>();
         buildTable(size,node);
     }
 
+
     @Override
-    public void insert(HashTable<K, V> table, int hashFunction, V value, K key) {
+    public void insert(V value, K key, int index) {
+        int keyV = (int) key;
+        int i = hashFuntion(keyV,index);
+        HashNode<K,V> nodeToInsert = getNode(i,node);
+        if(nodeToInsert.getKey()==null && nodeToInsert.getValue()==null){
+            nodeToInsert.setKey(key);
+            nodeToInsert.setValue(value);
+        }else{
+            insert(value,key,index+1);
+        }
 
     }
 
 
     @Override
     public boolean remove(K key) {
-        return false;
+        HashNode<K,V> toRemove = getNodeBykey(key);
+        if(toRemove == null){
+            return false;
+        }else{
+            toRemove = new HashNode<>();
+            return true;
+        }
     }
 
-    @Override
-    public int count() {
-        return 0;
-    }
-
-    @Override
-    public boolean contains(K key) {
-        return false;
-    }
 
     @Override
     public void buildTable(int size, HashNode<K,V> hashNode) {
@@ -40,6 +49,40 @@ public class HashTable<K,V>  implements IHashTable<K,V> {
         }
     }
 
+    @Override
+    public int hashFuntion(int key, int i) {
+        return  (key+i)%size;
+    }
 
+    @Override
+    public HashNode<K, V> getNodeBykey(K key) {
+        return getNodeByKey(key,node);
+    }
 
+    private HashNode<K,V>getNodeByKey(K key, HashNode<K,V> current){
+        if(current.getKey()==key){
+            return current;
+        }else{
+            return getNodeByKey(key, current.getNext());
+        }
+    }
+
+    //Obtiene el primero
+    public HashNode<K, V> getNode() {
+        return node;
+    }
+
+    //Obtiene los demas
+    public HashNode<K, V> getNode(int index, HashNode<K,V> current) {
+        if(index!=0){
+              current = current.getNext();
+             return getNode(index-1,current);
+        }else{
+            return current;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
--- src/model/ownImplementation/interfaces/IHashTable.java
+++ src/model/ownImplementation/interfaces/IHashTable.java
@@ -1,12 +1,12 @@
 package model.ownImplementation.interfaces;
 
 import model.ownImplementation.classes.HashNode;
-import model.ownImplementation.classes.HashTable;
+
 
 public interface IHashTable<K,V> {
-    void insert(HashTable<K,V> table, int hashFunction, V value, K key);
+    void insert(V value, K key, int index);
     boolean remove(K key);
-    int count();
-    boolean contains(K key);
     void buildTable(int size, HashNode<K,V> hashNode);
+    int hashFuntion(int key, int index);
+    HashNode<K,V> getNodeBykey(K key);
 }
