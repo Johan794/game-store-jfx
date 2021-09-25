cd4d8e299588cf6258f9406e100bd7d16e63f323
danieljaraba <daesjaga2003@gmail.com>
2021-09-24 19:52:45 -0500

    Changing hash to arraylist

--- src/model/source/Shelve.java
+++ src/model/source/Shelve.java
@@ -3,28 +3,28 @@ package model.source;
 
 import model.ownImplementation.classes.HashTable;
 
+import java.util.ArrayList;
+
 public class Shelve {
 
     private String code;
-    private HashTable<Integer, Game> games;
+    private ArrayList<Game> games;
 
     public Shelve(String code, int size){
         this.code = code;
-        this.games = new HashTable<>(size);
+        this.games = new ArrayList<>();
     }
 
     public void addGame(int gCode, int price, int quantity){
         Game game = new Game(gCode, price, quantity);
-        games.insert(game, gCode, 0);
+        games.add(game);
     }
 
     public boolean isGame(int gCode){
         boolean found = false;
-        for(int i = 0; i<games.getSize() && !found; i++){
-            if(games.getNodeByIndex(i) != null){
-                if(games.getNodeByIndex(i).getValue().getCode() == gCode){
-                    found = true;
-                }
+        for(int i = 0; i<games.size() && !found; i++){
+            if(games.get(i).getCode() == gCode){
+                found = true;
             }
         }
         return found;
@@ -33,14 +33,16 @@ public class Shelve {
     public Game findGame(int gCode){
         boolean found = false;
         Game game = null;
-        for(int i = 0; i<games.getSize() && !found; i++){
-            if(games.getNodeByIndex(i) != null){
-                if(games.getNodeByIndex(i).getValue().getCode() == gCode){
-                    game = games.getNodeByIndex(i).getValue();
-                    found = true;
-                }
+        for(int i = 0; i<games.size() && !found; i++){
+            if(games.get(i).getCode() == gCode){
+                game = games.get(i);
+                found = true;
             }
         }
         return game;
+    }
+
+    public String getCode(){
+        return code;
     }
 }
